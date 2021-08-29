85adfca0df726a904bc73f7991506ef994d48d9e
Nate Sales <nate@natesales.net>
2021-08-28 17:19:41 -0700

    fix(db): create uuid extension and run migrations

--- internal/db/db.go
+++ internal/db/db.go
@@ -11,13 +11,22 @@ func Connect(dsn string) (*gorm.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// Create UUID extension
+	err = db.Exec(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`).Error
+	if err != nil {
+		return nil, err
+	}
+
+	// Run schema migrations
 	if err := migrate(db); err != nil {
 		return nil, err
 	}
+
 	return db, nil
 }
 
 // migrate runs migrations on all models
 func migrate(db *gorm.DB) error {
-	return db.AutoMigrate(&User{})
+	return db.AutoMigrate(&User{}, &Zone{}, &Record{})
 }
