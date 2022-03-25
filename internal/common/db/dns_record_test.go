a15e376977cce3efef59ce465ede39149648bb35
Nate Sales <nate@natesales.net>
2022-03-24 23:32:48 -0700

    feat: add dns script record test

--- internal/common/db/dns_record_test.go
+++ internal/common/db/dns_record_test.go
@@ -74,3 +74,35 @@ func TestRecordUpdate(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "203.0.113.1", records[0].Value)
 }
+
+func TestDNSScriptRecordAddListDelete(t *testing.T) {
+	db, err := TestSetup()
+	assert.Nil(t, err)
+
+	// Add user1
+	err = UserAdd(db, "user1@example.com", "password1", "example referrer")
+	assert.Nil(t, err)
+
+	err = ZoneAdd(db, "example1.com", "user1@example.com")
+	assert.Nil(t, err)
+	example1, err := ZoneFind(db, "example1.com")
+	assert.Nil(t, err)
+	assert.NotNil(t, example1)
+
+	err = RecordAdd(db, &Record{
+		Type:   "DNSSCRIPT",
+		Label:  "@",
+		Value:  "async function handleQuery(query) {}",
+		TTL:    86400,
+		ZoneID: example1.ID,
+	})
+	assert.Nil(t, err)
+
+	records, err := RecordList(db, example1.ID)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(records))
+
+	deleted, err := RecordDelete(db, records[0].ID)
+	assert.Nil(t, err)
+	assert.True(t, deleted)
+}
